7a7a8e63c308bf69bad5a32ea17cca38286ccc1f
NAO5802 <i100185b@gmail.com>
2024-08-07 23:02:12 +0900

    Extract variables

--- refactoring/catalog.js
+++ refactoring/catalog.js
@@ -38,9 +38,9 @@ function reportLines(aCustomer) {
 export function price(order) {
     //price = base price − quantity discount + shipping
     const basePrice = order.quantity * order.itemPrice;
-
+    const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
     return basePrice -
-        Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
+        quantityDiscount +
         Math.min(basePrice * 0.1, 100);
 }
 
