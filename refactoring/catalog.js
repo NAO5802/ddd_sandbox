3be8e1abf66e25c2625698df9be04145e1733f0c
NAO5802 <i100185b@gmail.com>
2025-02-18 08:44:54 +0900

    Inline method

---
+++ refactoring/catalog.js
@@ -0,0 +1,40 @@
+// Extract method
+export function printOwing(invoice) {
+    const outstanding = calculateOutstanding(invoice);
+    invoice.dueDate = recordDueDate();
+
+    return printResult(invoice, outstanding);
+
+    function calculateOutstanding(invoice) {
+        let outstanding = 0;
+        for (const o of invoice.orders) {
+            outstanding += o.amount;
+        }
+        return outstanding;
+    }
+
+    function recordDueDate() {
+        const today = Clock.today;
+        return new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+    }
+
+    function printResult(invoice, outstanding) {
+        return `***********************\n**** Customer Owes ****\n***********************\nname: ${invoice.customer}\namount: ${outstanding}\ndue: ${invoice.dueDate.toLocaleDateString()}`;
+    }
+}
+
+class Clock {
+    static today = new Date('2024-06-30');
+}
+
+// Inline method
+function reportLines(aCustomer) {
+    const lines = [];
+    lines.push(["name", aCustomer.name]);
+    lines.push(["location", aCustomer.location]);
+    return lines;
+}
+
+
+
+
